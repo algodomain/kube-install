ca0ce64daaffbe0c670ceea3825fbd83abdef4dc
ad-sarang <148765889+ad-sarang@users.noreply.github.com>
2024-07-24 15:17:14 +0530

    Updated kube-install.sh

--- kube-install.sh
+++ kube-install.sh
@@ -17,38 +17,15 @@ echo \
 
 # Update package list and install Docker
 sudo apt-get update
-sudo apt-get install -y docker-ce
+sudo apt-get install -y containerd.io
 
-# Install cri-dockerd
-LATEST_VERSION=$(wget -qO- https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest | jq -r '.tag_name')
-echo "The latest version is: $LATEST_VERSION"
+sudo containerd config default > /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+systemctl restart containerd
+systemctl enable containerd >/dev/null
 
-wget https://github.com/Mirantis/cri-dockerd/releases/download/$LATEST_VERSION/cri-dockerd-${LATEST_VERSION:1}.amd64.tgz
-tar -xzf cri-dockerd-${LATEST_VERSION:1}.amd64.tgz
-sudo mv cri-dockerd/cri-dockerd /usr/local/bin/
-sudo chmod +x /usr/local/bin/cri-dockerd
-
-# Create a systemd service file for cri-dockerd
-cat <<EOF | sudo tee /etc/systemd/system/cri-dockerd.service > /dev/null
-[Unit]
-Description=CRI Docker Daemon
-Documentation=https://github.com/Mirantis/cri-dockerd
-Wants=docker.socket
-After=docker.socket
-
-[Service]
-ExecStart=/usr/local/bin/cri-dockerd
-Restart=always
-RestartSec=10s
-TimeoutSec=5min
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Reload systemd and enable the cri-dockerd service
-sudo systemctl daemon-reload
-sudo systemctl enable cri-dockerd
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo sh -c 'echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf'
 
 # Add Kubernetes GPG key and repository
 sudo mkdir -p /etc/apt/keyrings
@@ -63,18 +40,5 @@ sudo apt-mark hold kubelet kubeadm kubectl
 # Permanently disable swap
 sudo swapoff -a
 sudo sed -i.bak '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-
-# Create kubeadm-config.yaml file
-cat <<EOF | sudo tee /etc/kubeadm-config.yaml > /dev/null
-apiVersion: kubeadm.k8s.io/v1beta3
-kind: InitConfiguration
-nodeRegistration:
-  criSocket: unix:///var/run/cri-dockerd.sock
----
-apiVersion: kubeadm.k8s.io/v1beta3
-kind: ClusterConfiguration
-networking:
-  podSubnet: 10.244.0.0/16
-EOF
 
 echo "Setup is complete!"
